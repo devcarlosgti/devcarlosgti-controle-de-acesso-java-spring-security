3e957e5db96707ed64ead8e1c8b6507b6f1a87fc
Carlos-GleysonTI <c.gleysonti@gmail.com>
2024-08-21 19:41:48 -0300

    Criptografando a senha e testando o fluxo de usuario e permissoes

--- src/main/java/io/github/devcarlosgti/sbootexp_security/domain/service/UsuarioService.java
+++ src/main/java/io/github/devcarlosgti/sbootexp_security/domain/service/UsuarioService.java
@@ -7,7 +7,9 @@ import io.github.devcarlosgti.sbootexp_security.domain.repository.GrupoRepositor
 import io.github.devcarlosgti.sbootexp_security.domain.repository.UsuarioGrupoRepository;
 import io.github.devcarlosgti.sbootexp_security.domain.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,22 +22,24 @@ public class UsuarioService {
     private final UsuarioRepository repository;
     private final GrupoRepository grupoRepository;
     private final UsuarioGrupoRepository usuarioGrupoRepository;
+    private final PasswordEncoder passwordEncoder; //p criptografar a senha
 
+    @Transactional
     public Usuario salvar(Usuario usuario, List<String> grupos){
+        String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(senhaCriptografada);
         repository.save(usuario);
 
-        List<UsuarioGrupo> listaUsuarioGrupo = grupos.stream().map(nomeGrupo ->{
-            Optional<Grupo> possivelGrupo = grupoRepository.findByNome(nomeGrupo);
-            //verificar
-            if(possivelGrupo.isPresent()){
-                Grupo grupo = possivelGrupo.get();
-                return  new UsuarioGrupo(usuario,grupo);
-            }
-            //se ñ encontrar
-            return null;
-        })
+        List<UsuarioGrupo> listaUsuarioGrupo = grupos.stream().map(nomeGrupo -> {
+                    Optional<Grupo> possivelGrupo = grupoRepository.findByNome(nomeGrupo);
+                    if (possivelGrupo.isPresent()) {
+                        Grupo grupo = possivelGrupo.get();
+                        return new UsuarioGrupo(usuario, grupo);
+                    }
+                    return null;
+                })
                 .filter(grupo -> grupo != null)
-                .collect(Collectors.toList()); //transformo uma lista
+                .collect(Collectors.toList());
 
         usuarioGrupoRepository.saveAll(listaUsuarioGrupo);
 
